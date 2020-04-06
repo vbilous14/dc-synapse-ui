3fa31b5febce308a19c8c703c7c1c08815d48abe
v.bilous <v.bilous.upwork@gmail.com>
2020-04-06 13:31:17 +0300

    showNotification refactoring

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,9 @@
-import React, {useCallback, useState} from 'react';
+import React, { useCallback } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Snackbar from '@material-ui/core/Snackbar';
+import { useDispatch } from 'react-redux';
+import { showNotification as showNotificationAction } from 'react-admin';
 import { makeStyles } from '@material-ui/core/styles';
-
 
 const useStyles = makeStyles({
     root: {
@@ -20,38 +20,23 @@ const useStyles = makeStyles({
 });
 
 const Profile = () => {
+    const dispatch = useDispatch();
     const classes = useStyles();
-    const [successMsg, setSuccessMsg] = useState('');
     const handleSubmit = useCallback(() => {
-        setSuccessMsg('Profile saved successfully');
-    }, []);
-    const handleClose = useCallback(() => {
-        setSuccessMsg();
+        dispatch(showNotificationAction('Profile saved successfully'));
     }, []);
 
     return (
-        <div>
-            <form className={classes.root} onSubmit={handleSubmit}>
-                <TextField label='Name' required />
-                <TextField label='Age' type='number' inputProps={{ min: '0', step: '1' }} required />
-                <TextField label='Job title' required />
-                <div>
-                    <Button className={classes.buttonWrapper} variant='contained' color='primary' type='submit'>
-                        Save
-                    </Button>
-                </div>
-            </form>
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'bottom'
-                }}
-                onClose={handleClose}
-                open={!!successMsg}
-                message={successMsg}
-                autoHideDuration={2000}
-            />
-        </div>
+        <form className={classes.root} onSubmit={handleSubmit}>
+            <TextField label='Name' required />
+            <TextField label='Age' type='number' inputProps={{ min: '0', step: '1' }} required />
+            <TextField label='Job title' required />
+            <div>
+                <Button className={classes.buttonWrapper} variant='contained' color='primary' type='submit'>
+                    Save
+                </Button>
+            </div>
+        </form>
     )
 };
 
--- src/themes.js
+++ src/themes.js
@@ -2,7 +2,6 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 import red from '@material-ui/core/colors/red';
 
-console.log(blue);
 export default createMuiTheme({
     palette: {
         primary: blue,
