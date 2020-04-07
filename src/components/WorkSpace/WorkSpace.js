f8441e6da8263198213b8e1164a874a12ed39460
v.bilous <v.bilous.upwork@gmail.com>
2020-04-07 18:54:50 +0300

    removed unnecessary

--- src/components/WorkSpace/WorkSpace.js
+++ src/components/WorkSpace/WorkSpace.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
